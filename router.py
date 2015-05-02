ea9f60a09a864e601186708b94081ff2079d85fa
Madh93 <michelangelo93@gmail.com>
2015-05-02 12:53:25 +0100

    comprobar arp request y reply en otro metodo

--- router.py
+++ router.py
@@ -73,6 +73,8 @@ class SimpleRouter(app_manager.RyuApp):
         print(1)
         if eth.ethertype==ether.ETH_TYPE_ARP: #Si se trata de un paquete ARPÇ
             print(2)
+            self.receive_arp(datapath, packet, ethertype, in_port)
+            '''
             arp_msg= packet.get_protocol(arp.arp)
             #print('Mostrar unos cuantos')
             if (arp_msg.dst_ip == self.ports_to_ips[in_port-1][0] and arp_msg.opcode==arp.ARP_REQUEST): #
@@ -95,6 +97,7 @@ class SimpleRouter(app_manager.RyuApp):
                 p.add_protocol(a)
                 self.send_packet(datapath, in_port, p)
                 print('Sen envió el paquete')
+            '''
         elif eth.ethertype==ether.ETH_TYPE_IP: #Si se trata de un paquete IP
             #print('paquete ip')
             print(4)
@@ -130,6 +133,42 @@ class SimpleRouter(app_manager.RyuApp):
                   actions=actions,
                   data=data)
         datapath.send_msg(out)
+
+    # Procesar si se trata de una respuesta o una peticion ARP
+    def receive_arp(self, datapath, packet, etherFrame, inPort):
+
+        arp_msg = packet.get_protocol(arp.arp)
+
+        if arp_msg.opcode == arp.ARP_REQUEST:
+            if arp_msg.dst_ip == self.ports_to_ips[inPort-1][0]:
+                print('Es un ARP_REQUEST al mismo PC')
+                e = ethernet.ethernet(dst=etherFrame.src, 
+                                        src=self.ports_to_ips[inPort-1][2], 
+                                        ethertype=ether.ETH_TYPE_ARP)
+                a = arp.arp(opcode=arp.ARP_REPLY, 
+                            src_mac=self.ports_to_ips[inPort-1][2], 
+                            src_ip=arp_msg.dst_ip, 
+                            dst_mac=etherFrame.src, 
+                            dst_ip=arp_msg.src_ip)
+            else:
+                print('Es un ARP_REQUEST a otro PC') 
+                e = ethernet.ethernet(dst=etherFrame.dst, 
+                                        src=etherFrame.src, 
+                                        ethertype=ether.ETH_TYPE_ARP)
+                a = arp.arp(opcode=arp.ARP_REQUEST, 
+                            src_mac=arp_msg.src_mac, 
+                            src_ip=arp_msg.src_ip, 
+                            dst_mac=etherFrame.dst, 
+                            dst_ip=arp_msg.dst_ip)
+
+            p = Packet()
+            p.add_protocol(e)
+            p.add_protocol(a)
+            self.send_packet(datapath, inPort, p)
+            print('Se envió el paquete')
+
+        elif arp_msg.opcode == arp.ARP_REPLY:
+            print('Es un ARP_REPLY')        
 
     def receive_ip(self, datapath, packet, etherFrame, inPort): #Función que se usa cuando se recibe un paquete IP
         #print('Hola')
@@ -208,7 +247,7 @@ class SimpleRouter(app_manager.RyuApp):
         else:
         	print('No se ha enviado nada')
         '''
-        if find_in_routingTable(dstIp):
+        if self.find_in_routingTable(dstIp):
             self.send_icmp(datapath, dstMac, dstIp, srcMac, srcIp, inPort, seq, data, id, 0, ttl)
         else:
             print('No se ha enviado nada')
